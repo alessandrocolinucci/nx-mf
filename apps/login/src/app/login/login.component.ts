bc324f01a5d9b896711102786c936081568fac0c
Alessandro Colinucci <info@acolinucci.com>
2023-07-17 15:49:46 +0200

    feat(PrimeNG): PrimeNG added to projects and to Login

--- apps/login/src/app/login/login.component.ts
+++ apps/login/src/app/login/login.component.ts
@@ -1,11 +1,16 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+} from '@angular/core';
 import {
   FormControl,
   FormGroup,
   FormsModule,
   ReactiveFormsModule,
 } from '@angular/forms';
+import { PasswordModule } from 'primeng/password';
 
 type LoginForm = {
   email: FormControl<string>;
@@ -15,9 +20,11 @@ type LoginForm = {
 @Component({
   selector: 'nx-mf-login',
   standalone: true,
-  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, PasswordModule],
   templateUrl: './login.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
   loginForm: FormGroup = new FormGroup<LoginForm>({
