1df26b235f2908171c70f2593c18a4449cf2844c
Alessandro Colinucci <info@acolinucci.com>
2023-07-17 15:30:32 +0200

    feat(Login): Creadentials logic prepared

--- apps/login/src/app/login/login.component.ts
+++ apps/login/src/app/login/login.component.ts
@@ -1,11 +1,31 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+
+type LoginForm = {
+  email: FormControl<string>;
+  password: FormControl<string>;
+};
 
 @Component({
   selector: 'nx-mf-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginComponent {}
+export class LoginComponent {
+  loginForm: FormGroup = new FormGroup<LoginForm>({
+    email: new FormControl<string>('', { nonNullable: true }),
+    password: new FormControl<string>('', { nonNullable: true }),
+  });
+
+  login(): void {
+    console.log(this.loginForm.value);
+  }
+}
